02bd17a39bed56d36e62946bf4c9ec920672f989
Jaap Roes <jaap@eight.nl>
2013-07-01 11:10:03 +0200

    Reversed the order of the conf.urls imports, fallback is the old module path.

--- smuggler/urls.py
+++ smuggler/urls.py
@@ -7,9 +7,9 @@
 # Software Foundation. See the file README for copying conditions.
 
 try:
-    from django.conf.urls.defaults import url, patterns
-except:
     from django.conf.urls import url, patterns
+except ImportError:  # Django < 1.4
+    from django.conf.urls.defaults import url, patterns
                             
 from smuggler.views import (dump_data, dump_app_data, dump_model_data,
                             load_data)
