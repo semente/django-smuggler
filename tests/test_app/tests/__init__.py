d3834bbedd11d91561fd781ed4adaf34233e878b
Jaap Roes <jaap@eight.nl>
2014-08-18 15:16:27 +0200

    Test load_requested_data

--- tests/test_app/tests/__init__.py
+++ tests/test_app/tests/__init__.py
@@ -3,3 +3,4 @@ from .test_auth import *
 from .test_urls import *
 from .test_views import *
 from .test_forms import *
+from .test_load import *
---
+++ tests/test_app/tests/test_load.py
@@ -0,0 +1,54 @@
+import json
+from django.contrib.sites.models import Site
+from django.test import TestCase
+from smuggler.utils import load_requested_data
+from test_app.models import Page
+
+
+class SimpleLoadTestCase(TestCase):
+    PAGE_DUMP = [{
+        "pk": 1,
+        "model": "test_app.page",
+        "fields": {
+            "title": "test", "body": "test body",
+        }
+    }]
+    SITE_DUMP = [{
+        "pk": 1,
+        "model": "sites.site",
+        "fields": {
+            "domain": "example.com",
+            "name": "test.com"
+        }
+    }]
+    ALL_DUMP = PAGE_DUMP + SITE_DUMP
+
+    def test_load_page(self):
+        self.assertEqual(0, Page.objects.count())
+        count = load_requested_data([
+            ('json', json.dumps(self.PAGE_DUMP))
+        ])
+        self.assertEqual(count, 1)
+        self.assertEqual('test', Page.objects.get(pk=1).title)
+
+    def test_load_page_and_site(self):
+        self.assertEqual(0, Page.objects.count())
+        self.assertEqual(Site.objects.get(pk=1).name, 'example.com')
+        count = load_requested_data([
+            ('json', json.dumps(self.PAGE_DUMP)),
+            ('json', json.dumps(self.SITE_DUMP))
+        ])
+        self.assertEqual(count, 2)
+        self.assertEqual('test', Page.objects.get(pk=1).title)
+        self.assertEqual('test.com', Site.objects.get(pk=1).name)
+
+    def test_load_all(self):
+        self.assertEqual(0, Page.objects.count())
+        self.assertEqual(Site.objects.get(pk=1).name, 'example.com')
+        count = load_requested_data([
+            ('json', json.dumps(self.ALL_DUMP))
+        ])
+        self.assertEqual(count, 2)
+        self.assertEqual('test', Page.objects.get(pk=1).title)
+        self.assertEqual('test.com', Site.objects.get(pk=1).name)
+
