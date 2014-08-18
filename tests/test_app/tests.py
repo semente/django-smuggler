21120bb9f8e7a65c7fda11245ed579ab35c1bc65
Jaap Roes <jaap@eight.nl>
2014-08-18 10:10:05 +0200

    Removed dependency on django.contrib.flatpages and introduced our own model

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,42 +1,53 @@
-import re
-import StringIO
-
-from django.contrib.flatpages.models import FlatPage
+import json
+from django.utils.six import StringIO
 from django.core.management import CommandError
 from django.test import TestCase
-from unittest2 import TestCase as TestCase2
+from test_app.models import Page
 from smuggler import utils
 
 
-class BasicDumpTestCase(TestCase, TestCase2):
-    SITE_DUMP = '{ "pk": 1, "model": "sites.site", "fields": { "domain": "example.com", "name": "example.com" } }'
-    FLATPAGE_DUMP = '{ "pk": 1, "model": "flatpages.flatpage", "fields": { "registration_required": false, "title": "test", "url": "/", "template_name": "", "sites": [], "content": "", "enable_comments": false } }'
-    BASIC_DUMP = '[ %s, %s ]' % (SITE_DUMP, FLATPAGE_DUMP)
+class BasicDumpTestCase(TestCase):
+    SITE_DUMP = [{
+        "pk": 1,
+        "model": "sites.site",
+        "fields": {
+            "domain": "example.com",
+            "name": "example.com"
+        }
+    }]
+    PAGE_DUMP = [{
+        "pk": 1,
+        "model": "test_app.page",
+        "fields": {
+            "title": "test", "body": "test body",
+        }
+    }]
+    BASIC_DUMP = SITE_DUMP + PAGE_DUMP
 
     def setUp(self):
-        f = FlatPage(url='/', title='test')
-        f.save()
+        p = Page(title='test', body='test body')
+        p.save()
 
     def normalize(self, out):
-        return re.sub(r'\s\s*', ' ', out).strip()
+        return json.loads(out)
 
     def test_serialize_to_response(self):
-        stream = StringIO.StringIO()
+        stream = StringIO()
         utils.serialize_to_response(response=stream)
         out = self.normalize(stream.getvalue())
-        self.assertEquals(out, self.BASIC_DUMP)
+        self.assertEqual(out, self.BASIC_DUMP)
 
     def test_serialize_exclude(self):
-        stream = StringIO.StringIO()
+        stream = StringIO()
         utils.serialize_to_response(exclude=['sites'], response=stream)
         out = self.normalize(stream.getvalue())
-        self.assertEquals(out, '[ %s ]' % self.FLATPAGE_DUMP)
+        self.assertEqual(out, self.PAGE_DUMP)
 
     def test_serialize_include(self):
-        stream = StringIO.StringIO()
+        stream = StringIO()
         utils.serialize_to_response(app_labels=['sites'], response=stream)
         out = self.normalize(stream.getvalue())
-        self.assertEquals(out, '[ %s ]' % self.SITE_DUMP)
+        self.assertEqual(out, self.SITE_DUMP)
 
     def test_serialize_unknown_app_fail(self):
         self.assertRaises(CommandError, utils.serialize_to_response, 'auth')
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,7 +1,3 @@
-# Haystack settings for running tests.
-DATABASE_ENGINE = 'django.db.backends.sqlite3'
-DATABASE_NAME = 'smuggler.db'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -13,7 +9,6 @@ SECRET_KEY = 'mAtTzVPOV9JY4eJQfqgW8eAS9DWKnt3MkvvpQI2MzkhAz7z3'
 
 INSTALLED_APPS = [
     'django.contrib.sites',
-    'django.contrib.flatpages',
     'smuggler',
     'test_app',
 ]
