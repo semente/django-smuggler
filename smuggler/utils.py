f3a1499dbc3ff8f94d899f777e9403d3837e1a99
Mikhail Fedosov <tbs.micle@gmail.com>
2015-09-20 13:12:44 +0300

    add `use_natural_keys` for Django <1.7

--- smuggler/utils.py
+++ smuggler/utils.py
@@ -43,6 +43,7 @@ def serialize_to_response(app_labels=[], exclude=[], response=None,
             'exclude': exclude,
             'format': format,
             'indent': indent,
+            'use_natural_keys': True,
             'use_natural_foreign_keys': True,
             'use_natural_primary_keys': True
         })
