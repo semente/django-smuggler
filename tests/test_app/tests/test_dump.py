c0d4c4c4c762eb5cfc332a2c078298a10581ae36
Jaap Roes <jroes@leukeleu.nl>
2016-11-08 13:29:34 +0100

    Fix tox.ini and fix failing test

--- tests/test_app/tests/test_dump.py
+++ tests/test_app/tests/test_dump.py
@@ -32,7 +32,10 @@ class BasicDumpTestCase(TestCase):
         p.save()
 
     def normalize(self, out):
-        return json.loads(out)
+        data = json.loads(out)
+        if data[0]['model'] == 'sites.site' and 'pk' in data[0]:
+            del data[0]['pk']  # Django < 1.10 dumps pk
+        return data
 
     def test_serialize_exclude(self):
         stream = StringIO()
