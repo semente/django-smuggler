13f1d3a3fbc54cf666aa0f99fe95570f92618881
Jaap Roes <jroes@leukeleu.nl>
2020-08-06 14:26:14 +0200

    Admin now requires request context processor

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages'
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ),
         },
     },
